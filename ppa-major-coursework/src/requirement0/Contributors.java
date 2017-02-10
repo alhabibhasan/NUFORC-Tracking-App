4aa264c8b714dd20c80e2e690eeac987fd9007de
Muhammed Hasan <Muhammedahasan@outlook.com>
2017-02-10 20:57:13 +0000

    Initial Commit of GUI branch

--- ppa-major-coursework/src/requirement0/Contributors.java
+++ ppa-major-coursework/src/requirement0/Contributors.java
@@ -1,5 +1,5 @@
 package requirement0;
-
+import api.ripley.*;
 public class Contributors {
 
 	public static void main(String[] args) {
@@ -7,5 +7,13 @@ public class Contributors {
 		System.out.println("Aflal Asker");
 		System.out.println("Baha Aboukoura");
 		System.out.println("Jaman Salique");
+		
+		
+		//Ripley(String privateKey, String publicKey)
+		Ripley test = new Ripley("10tLI3CRs9qyVD6ql2OMtA==", "tBgm4pRv9wrVqL46EnH7ew==");
+		
+		System.out.println(test.getLastUpdated().toString());
+		
+				
 	}
 }
