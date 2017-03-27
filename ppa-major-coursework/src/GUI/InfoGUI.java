b760c436895baa45063c1ccef224c57cfba814c5
k1630580 <k1630580@nmscde000845.nms.kcl.ac.uk>
2017-03-27 11:39:31 +0100

    Recreated the info gui with MVC in mind

--- ppa-major-coursework/src/GUI/InfoGUI.java
+++ ppa-major-coursework/src/GUI/InfoGUI.java
@@ -1,7 +1,10 @@
 package GUI;
 
 import java.awt.BorderLayout;
-import java.util.ArrayList;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JComboBox;
@@ -10,101 +13,105 @@ import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.ListSelectionModel;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
+import javax.swing.ScrollPaneConstants;
 
 import Data.CustomIncident;
-import Data.Process;
+import controllers.InfoGUIListener;
 
-public class InfoGUI extends JFrame {
+public class InfoGUI extends JFrame implements Observer {
 
-	private JFrame frame;
-	private JPanel north, center;
-	private JComboBox<String> comboBox;
-	private JScrollPane pane;
-	private JList list;
-	private static String[] stringArr;
-	private static CustomIncident[] incidentArr;
-	private String stateAbrev, stateName;
-	private DefaultListModel dList;
-	
-	/**
-	 * Creates a JFrame which display the relevant information
-	 * @param stateAbrev The abbreviation of the state about which info. should be displayed.
-	 * @param stateName The name of the state in question.
-	 */
-	public InfoGUI(String stateAbrev, String stateName) {
-		this.stateAbrev = stateAbrev;
-		this.stateName = stateName;
-		dList = new DefaultListModel();
-		frame = new JFrame();
-		north = new JPanel();
-		center = new JPanel();
-		comboBox = new JComboBox<String>();
-		createGUI();
-	}
-	
-	
-	private void createGUI() {
+  private JFrame frame;
+  private JComboBox<String> comboBox;
+  private JScrollPane pane;
+  private JList<String> list;
+  private String stateAbrev, stateName;
+  private DefaultListModel<String> listModel;
+  private InfoGUIListener controller;
 
-		frame.setLayout(new BorderLayout());
-		frame.setTitle(stateName + " " + "(" + stateAbrev + ")");
-		frame.getContentPane().add(center, BorderLayout.CENTER);
-		frame.getContentPane().add(north, BorderLayout.NORTH);
-		north.add(comboBox);
+  /**
+   * Creates a JFrame which display the relevant information
+   * 
+   * @param stateAbrev
+   *            The abbreviation of the state about which info. should be
+   *            displayed.
+   * @param stateName
+   *            The name of the state in question.
+   */
+  public InfoGUI(String stateAbrev, String stateName) {
+    this.stateAbrev = stateAbrev;
+    this.stateName = stateName;
+    this.listModel = new DefaultListModel<String>();
+    this.frame = new JFrame();
+    this.comboBox = new JComboBox<String>();
+    this.controller = new InfoGUIListener(listModel, comboBox, this);
 
-		addComboBoxElements();
+    this.list = new JList<String>(listModel);
+    this.pane = new JScrollPane(list);
 
-		frame.setVisible(true);
+    list.addListSelectionListener(e -> {
+      if (!list.isSelectionEmpty()) {
+      JOptionPane.showMessageDialog(new JFrame(), controller.getSummaryFor(list.getSelectedIndex()),
+          "Summary of incident", JOptionPane.INFORMATION_MESSAGE);
+      list.clearSelection();
+      }
+    });
 
-		setupList();
-		frame.pack();
-	}
+    this.controller.getDefaultData(stateAbrev);
 
-	private void addComboBoxElements() {
-		String Date = "Date";
-		String City = "City";
-		String Shape = "Shape";
-		String Duration = "Duration";
-		String Posted = "Posted";
+    this.createGUI();
+  }
 
-		comboBox.addItem(Date);
-		comboBox.addItem(City);
-		comboBox.addItem(Shape);
-		comboBox.addItem(Duration);
-		comboBox.addItem(Posted);
+  private void createGUI() {
 
-	}
+    frame.setLayout(new BorderLayout());
+    frame.setTitle(stateName + " " + "(" + stateAbrev + ")");
 
-	private void setupList() {
+    frame.add(comboBox, BorderLayout.NORTH);
+    frame.add(pane, BorderLayout.CENTER);
 
-		ArrayList<CustomIncident> allIncids = Process.sortListForState(stateAbrev);
-		incidentArr = new CustomIncident[allIncids.size()];
-		stringArr = new String[allIncids.size()];
-		allIncids.toArray(incidentArr);
-		String temp;
-		for (int i = 0; i <= incidentArr.length - 1; i++) {
-			stringArr[i] = "Date and Time: " + incidentArr[i].getDateAndTime() + " " + "City: " + incidentArr[i].getCity() + " "
-					+"Duration: " + incidentArr[i].getDuration() + " " + "Posted: " + incidentArr[i].getPosted();
-			temp = 	 "Date and Time: " + incidentArr[i].getDateAndTime() + " " + "City: " + incidentArr[i].getCity() + " "
-					+"Duration: " + incidentArr[i].getDuration() + " " + "Posted: " + incidentArr[i].getPosted();
-			dList.addElement(temp);
-		}
-		
-		
-		list = new JList(dList);
-		list.setVisibleRowCount(20);
-		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+    addComboBoxElements();
+    frame.setVisible(true);
+    frame.pack();
+  }
 
-		list.addListSelectionListener(new ListSelectionListener() {
-			public void valueChanged(ListSelectionEvent event) {
-				JOptionPane.showMessageDialog(new JFrame(), incidentArr[list.getSelectedIndex()].getSummary());
-			}
-		}
+  private void addComboBoxElements() {
 
-		);
-		center.add(new JScrollPane(list));
-	}
+    comboBox.addItem("-");
+    comboBox.addItem("Date");
+    comboBox.addItem("City");
+    comboBox.addItem("Shape");
+    comboBox.addItem("Duration");
+    comboBox.addItem("Posted");
+
+    comboBox.addActionListener(e -> {
+      
+      if (((String) comboBox.getSelectedItem()).equals("-")) {
+        controller.getDefaultData(stateAbrev);
+      }
+      if (((String) comboBox.getSelectedItem()).equals("Duration")) {
+        controller.getDurationSortedData();
+      }
+      if (((String) comboBox.getSelectedItem()).equals("Posted")) {
+        controller.getPostedSortedData();
+      }
+      if (((String) comboBox.getSelectedItem()).equals("City")) {
+        controller.getCitySortedData();
+      }
+      if (((String) comboBox.getSelectedItem()).equals("Date")) {
+        controller.getDateTimeSortedData();
+      }
+      if (((String) comboBox.getSelectedItem()).equals("Shape")) {
+        controller.getShapeSortedData();
+      }
+      
+      
+    });
+
+  }
+
+  @Override
+  public void update(Observable o, Object arg) {
+    listModel = controller.getListModel();
+  }
 
 }
