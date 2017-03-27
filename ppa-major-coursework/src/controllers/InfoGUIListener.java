8c20b351da6db8fedfd23e38b54c738d7db2b0a8
k1630580 <k1630580@nmscde000845.nms.kcl.ac.uk>
2017-03-27 12:24:30 +0100

    JavaDoc comments

--- ppa-major-coursework/src/controllers/InfoGUIListener.java
+++ ppa-major-coursework/src/controllers/InfoGUIListener.java
@@ -5,7 +5,11 @@ import javax.swing.JComboBox;
 
 import Data.Sorts;
 import GUI.InfoGUI;
-
+/**
+ * This class is the controller for the InfoGUI and the Sorts class.
+ * @author k1630580
+ *
+ */
 public class InfoGUIListener {
 	private JComboBox<String> comboBox;
 	private DefaultListModel<String> listModel;
@@ -25,6 +29,10 @@ public class InfoGUIListener {
 		this.sortedDataSource = new Sorts(gui);
 	}
 
+	/**
+	 * Returns the most current list model which is to be used by the view i.e. InfoGUI
+	 * @return
+	 */
 	public DefaultListModel<String> getListModel() {
 		return this.listModel;
 	}
@@ -47,6 +55,9 @@ public class InfoGUIListener {
 
 	}
 
+	/**
+	 * Tells the Sorts class to sort data based on the date of posting
+	 */
 	public void getPostedSortedData() {
 		listModel.clear();
 
@@ -55,6 +66,9 @@ public class InfoGUIListener {
 		}
 	}
 
+	/**
+	 * Tells the Sorts class to sort the current data based on the city the incident took place in.
+	 */
 	public void getCitySortedData() {
 		listModel.clear();
 
@@ -63,6 +77,9 @@ public class InfoGUIListener {
 		}
 	}
 	
+	/**
+	 * Tells the Sorts class to sort the current data based on the date and time the incident occured.
+	 */
 	public void getDateTimeSortedData() {
 		listModel.clear();
 
@@ -70,7 +87,10 @@ public class InfoGUIListener {
 			listModel.addElement(ci);
 		}
 	}
-
+	/**
+	 * Tells the sorts class to sort the current data based on the shape of the UFO(s) spotted in
+	 * each incident
+	 */
 	public void getShapeSortedData() {
 		listModel.clear();
 
@@ -79,13 +99,22 @@ public class InfoGUIListener {
 		}
 	}
 	
+	/**
+	 * Tells the Sorts class to return data as it was received from the API i.e. in random unsorted order
+	 * @param stateAbrev To state for which data should be returned
+	 */
 	public void getDefaultData(String stateAbrev) {
 		listModel.clear();
 		for (String ci : sortedDataSource.getOutputDataForState(stateAbrev)) {
 			listModel.addElement(ci);
 		}
 	}
-
+	
+	/**
+	 * Gets the summary of a selected incident
+	 * @param selectedIndex The incident selected by the user
+	 * @return The String data for the summary of the incident
+	 */
 	public String getSummaryFor(int selectedIndex) {
 		return sortedDataSource.summaryForInci(selectedIndex);
 	}
