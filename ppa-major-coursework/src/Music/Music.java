a5d974a0626f5ff231f0490585b5961f04686794
Jaman Salique <jamanr@hotmail.co.uk>
2017-03-28 12:20:27 +0100

    Added images and music to res and created a class that will play music

---
+++ ppa-major-coursework/src/Music/Music.java
@@ -0,0 +1,50 @@
+package Music;
+
+import java.io.File;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Music {
+
+
+	
+	
+	public void play() {
+		
+		File sound = new File("images/music.wav");
+		
+		try{
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(sound));
+			clip.loop(100000000);
+			clip.start();
+			
+			Thread.sleep(clip.getLongFramePosition() /1000);
+			
+		}catch(Exception e){
+			
+		}
+		
+		
+	}
+	
+public void playPop() {
+		
+		File sound = new File("images/pop.wav");
+		
+		try{
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(sound));
+			clip.start();
+			
+			Thread.sleep(clip.getLongFramePosition() /1000);
+			
+		}catch(Exception e){
+			
+		}
+		
+		
+	}
+	
+}
