3b325fe0748971e8c843f9fd8acaf771d94b603d
Jaman Salique <jamanr@hotmail.co.uk>
2017-03-28 12:30:49 +0100

    Created constructor for the Panel 4 GUI

--- ppa-major-coursework/src/GUI/Panel4GUI.java
+++ ppa-major-coursework/src/GUI/Panel4GUI.java
@@ -1,9 +1,15 @@
 package GUI;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -39,5 +45,60 @@ public class Panel4GUI extends JFrame {
 		//Music field
 		private Music music;
 		
+		/**
+		 * The constructor for the class. This is where all the fields are initialised
+		 * @throws IOException
+		 */
+		public Panel4GUI() throws IOException {
+			screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+			width = screenSize.getWidth(); //assigning the width of the computer screen being used to width field
+			height = screenSize.getHeight(); //assigning the height of the computer screen being used to height field
+			aliens = new Aliens(width, height);
+			southPanel = new JPanel();
+			martinBuffered = ImageIO.read(new File("images/astronaut.png"));
+			steffenBuffered = ImageIO.read(new File("images/asadImage.png"));
+			asadBuffered = ImageIO.read(new File("images/martinUFO.png"));
+			martinImage = new JLabel(new ImageIcon(martinBuffered));
+			steffenImage = new JLabel(new ImageIcon(steffenBuffered.getScaledInstance(150,300, steffenBuffered.SCALE_FAST)));
+			asadImage = new JLabel(new ImageIcon(asadBuffered.getScaledInstance(300,150, asadBuffered.SCALE_FAST)));
+			changeSpeed = new JLabel("< Change Speed!");
+			clear = new JButton("Clear Screen!");
+			addMartin = new JButton("Add Martin!");
+			addSteffen = new JButton("Add Steffen!");
+			addAsad = new JButton("Add Asad!");
+			startAnimation = new JButton("Start Animation!");
+			speedSlider = new JSlider(1, 50, 2);
+			sliderListener = new sliderListener(aliens);
+			random = new Random();
+			northPanel = new JPanel();
+			
+			/*
+			 * This paintComponent method is being used to set a background image
+			 * to the center JPanel
+			 */
+			centerPanel = new JPanel(){
+		        public void paintComponent(Graphics g) {
+		            super.paintComponent(g);
+		            ImageIcon space = new ImageIcon("images/space3.png");
+		            space.paintIcon(this, g, 0, 0);
+		        }
+		        
+			};
+			
+			/*
+			 * This paintComponent method is being used to set a background image
+			 * to the south JPanel
+			 */
+			southPanel = new JPanel(){
+		        public void paintComponent(Graphics g) {
+		            super.paintComponent(g);
+		            ImageIcon space = new ImageIcon("images/space3.png");
+		            space.paintIcon(this, g, 0, 0);
+			
+		        }	
+			};
+			music = new Music();
+			
+		}
 
 }
