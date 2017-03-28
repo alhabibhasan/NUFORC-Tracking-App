9485bb3481c3c557ff83b140fd21888b6afd0155
Jaman Salique <jamanr@hotmail.co.uk>
2017-03-28 12:29:00 +0100

    Added fields to the Panel 4 gui class

--- ppa-major-coursework/src/GUI/Panel4GUI.java
+++ ppa-major-coursework/src/GUI/Panel4GUI.java
@@ -1,9 +1,43 @@
 package GUI;
 
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+
+import controllers.sliderListener;
+import Aliens.Aliens;
+import Music.Music;
 
 public class Panel4GUI extends JFrame {
 	
-	
+	//JPanel fields
+		private JPanel centerPanel, southPanel, northPanel;
+		//Jlabel fields
+		private JLabel martinImage, steffenImage, asadImage, changeSpeed;
+		//BufferedImage fields
+		private BufferedImage martinBuffered, steffenBuffered, asadBuffered;
+		//JButton fields
+		private JButton clear, addMartin, addSteffen, addAsad,startAnimation;
+		//JSlider field
+		private JSlider speedSlider;
+		///Alien field
+		private Aliens aliens;
+		//sliderListener field
+		private sliderListener sliderListener;
+		//Random field
+		private Random random;
+		//Dimension field
+		private Dimension screenSize;
+		//double fields
+		private double width,height;
+		//Music field
+		private Music music;
+		
 
 }
